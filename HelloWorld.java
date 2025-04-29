ab5f2e06102971c15fcd04b23fe2c12aeae48a10
agongbread <695895611@qq.com>
2025-04-29 09:39:24 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        //输出相关内容
+        System.out.println("Hello World!");
+        System.out.println("We are students!");
+    }
+}
